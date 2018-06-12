2538ba4b8f9da7190e5d9cd046ec0cb8599a4b62
Kyle Nickel <nickelkr@gmail.com>
2018-06-12 17:02:30 -0400

    No duplicates allowed

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,5 @@
 class Film < ApplicationRecord
   belongs_to :user
+
+  validates :external_id, uniqueness: true
 end
--- test/models/film_test.rb
+++ test/models/film_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class FilmTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'ensure no duplicates' do
+    user = users(:kyle)
+
+    assert_no_difference('Film.count') do
+      user.films.create(title: 'New name', runtime: 14, external_id: films(:one).external_id)
+    end
+  end
 end
