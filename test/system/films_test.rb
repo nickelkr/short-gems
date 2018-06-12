112f27d49104dd29915fc780767e6a7ca2b5a36a
Kyle Nickel <nickelkr@gmail.com>
2018-06-12 17:19:19 -0400

    Fixed system test

--- test/system/films_test.rb
+++ test/system/films_test.rb
@@ -14,7 +14,7 @@ class FilmsTest < ApplicationSystemTestCase
 
     fill_in 'film_title', with: 'Hotel Chevalier'
     fill_in 'film_runtime', with: 12
-    fill_in 'film_external_id', with: 'https://youtu.be/HkqIVdMt_bs'
+    fill_in 'film_external_id', with: 'https://youtu.be/HkqIVdMt_bs2'
 
     assert_difference('Film.count') do
       click_on 'Submit'
