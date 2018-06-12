4f0e4fa47f8c1e3dbf83fc645c67fc5156cd0e94
Kyle Nickel <nickelkr@gmail.com>
2018-06-12 17:17:02 -0400

    Extracting ID

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,11 @@ class Film < ApplicationRecord
   belongs_to :user
 
   validates :external_id, uniqueness: true
+  
+  before_validation :extract_id
+
+  private
+    def extract_id
+      self.external_id = external_id.split('/').last
+    end
 end
--- test/models/film_test.rb
+++ test/models/film_test.rb
@@ -8,4 +8,13 @@ class FilmTest < ActiveSupport::TestCase
       user.films.create(title: 'New name', runtime: 14, external_id: films(:one).external_id)
     end
   end
+
+  test 'strip domain information' do
+    user = users(:kyle)
+    link = 'https://youtu.be/HkqIVdMt_bs2'
+
+    user.films.create(title: 'Hotel', runtime: 10, external_id: link)
+
+    assert_equal('HkqIVdMt_bs2', Film.last.external_id)
+  end
 end
