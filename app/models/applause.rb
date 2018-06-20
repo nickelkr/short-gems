ef6817cb5560530b9cbfe7c42b1d4b50a55a6e1c
Kyle Nickel <nickelkr@gmail.com>
2018-06-20 16:12:13 -0400

    Rearranging some code

--- app/models/applause.rb
+++ app/models/applause.rb
@@ -2,8 +2,8 @@ class Applause < ApplicationRecord
   belongs_to :film
   belongs_to :user
 
-  enum category: %i[directing story cinematography sound acting]
-
   validates :category, presence: true
   validates :category, uniqueness: { scope: [:film_id, :user_id] }
+
+  enum category: %i[directing story cinematography sound acting]
 end
