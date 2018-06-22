41df8f708626388235880439f0d1e36fa3680d5b
Kyle Nickel <nickelkr@gmail.com>
2018-06-21 23:01:06 -0400

    Applause UI test

---
+++ test/system/applauses_test.rb
@@ -0,0 +1,49 @@
+require 'application_system_test_case'
+
+class ApplausesTest < ApplicationSystemTestCase
+  KYLE_PASS = '12345678'
+  test 'adding a applause' do
+    user = users(:kyle)
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: KYLE_PASS
+    click_on 'Log in'
+
+    assert_difference('Applause.count') do
+      find(:xpath, "//button[@data-film-id='#{films(:one).id}' and @data-category='story']").click
+    end
+
+    applause = films(:one).applauses.last
+    assert_equal(user, applause.user)
+    assert_equal('story', applause.category)
+  end
+
+  test 'removing a applause' do
+    user = users(:kyle)
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: KYLE_PASS
+    click_on 'Log in'
+
+    assert_difference('Applause.count') do
+      find(:xpath, "//button[@data-film-id='#{films(:one).id}' and @data-category='story']").click
+    end
+
+    assert_difference('Applause.count', -1) do
+      find(:xpath, "//button[@data-film-id='#{films(:one).id}' and @data-category='story']").click
+    end
+  end
+
+  test 'total count' do
+  end
+
+  test 'require user to be logged in' do
+    visit films_path
+
+    find(:xpath, "//button[@data-film-id='#{films(:one).id}' and @data-category='sound']").click
+
+    assert_redirected_to new_user_session_path
+  end
+end
