9ef3ba7b7945cd854ead9feb41d0245d551d646a
Kyle Nickel <nickelkr@gmail.com>
2018-07-08 21:10:25 -0400

    Testing for runtime rules

--- test/models/film_test.rb
+++ test/models/film_test.rb
@@ -8,4 +8,28 @@ class FilmTest < ActiveSupport::TestCase
       user.films.create(title: 'New name', runtime: 14, external_id: films(:one).external_id)
     end
   end
+
+  test 'no runtimes under 0 minutes' do
+    user = users(:kyle)
+
+    assert_no_difference('Film.count') do
+      user.films.create(title: 'New name', runtime: -1, external_id: "#{films(:one).external_id}2")
+    end
+  end
+
+  test 'no runtimes over 50 minutes' do
+    user = users(:kyle)
+
+    assert_no_difference('Film.count') do
+      user.films.create(title: 'New name', runtime: 60, external_id: "#{films(:one).external_id}2")
+    end
+  end
+
+  test 'only numbers allowed in runtime' do
+    user = users(:kyle)
+
+    assert_no_difference('Film.count') do
+      user.films.create(title: 'New name', runtime: 'yellow', external_id: "#{films(:one).external_id}2")
+    end
+  end
 end
