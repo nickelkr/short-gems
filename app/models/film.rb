f0eeecc80dbe33d94b9baba69c3d22f01e29cd7e
Kyle Nickel <nickelkr@gmail.com>
2018-07-08 21:15:41 -0400

    Runtime validation for 0-50 minutes only

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,4 +3,8 @@ class Film < ApplicationRecord
   has_many :applauses, dependent: :destroy
 
   validates :external_id, uniqueness: true
+  validates :runtime, numericality: { greater_than: 0,
+                                      less_than_or_equal_to: 50,
+                                      only_integer: true ,
+                                      message: 'Runtime can only be whole numbers between 0 and 50 minutes.'}
 end
