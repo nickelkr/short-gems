7411a5f6701dc028542c92e37a1eb4f1e5ff99ab
Kyle Nickel <nickelkr@gmail.com>
2018-07-12 21:09:28 -0400

    Fixing broke ass tests

--- test/system/index_test.rb
+++ test/system/index_test.rb
@@ -9,7 +9,7 @@ class IndexTest < ApplicationSystemTestCase
     first_link = "https://www.youtube.com/embed/#{first_id}"
     second_link = "https://www.youtube.com/embed/#{second_id}"
 
-    assert page.has_xpath?("//iframe[@width='560' and @height='315' and @src='#{first_link}']")
-    assert page.has_xpath?("//iframe[@width='560' and @height='315' and @src='#{second_link}']")
+    assert page.has_xpath?("//iframe[@src='#{first_link}']")
+    assert page.has_xpath?("//iframe[@src='#{second_link}']")
   end
 end
